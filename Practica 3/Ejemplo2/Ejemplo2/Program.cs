5f378683df21fe5182015a167bcfdd60de95ee3a
Txedo <txedo@users.noreply.github.com>
2009-03-26 19:19:49 +0000

    * Añado proyecto del Ejemplo1 (código fuente, no subo ejecutables y archivos generados por MVS2008) * Añado proyecto del Ejemplo2 (código fuente, no subo ejecutables y archivos generados por MVS2008) * Añado imágenes que he incluido en la documentación * Añado manual de cómo crear un proyecto con MVS2008 (sección 3.1) * Añado sección de Conclusiones * Añado los ejemplos 1 y 2 que hemos hecho en clase de prácticas * Modificada la introducción de la sección 3 (Programación de la interfaz FTI) para cuadrar con la nueva sección 3.1 (Cómo crear un proyecto con MVS2008) * Actualizada la sección de Bibliografía * Índices actualizados

---
+++ Practica 3/Ejemplo2/Ejemplo2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejemplo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean hayEntrada = false;
+            // Creamos una instancia de la clase FishFace
+            FishFa30.FishFace fish = new FishFa30.FishFace();
+            try
+            {
+                // Abrimos la conexion con el interfaz
+                fish.OpenInterface(FishFa30.Port.COM1);
+                // Esperamos una señal por parte del sensor conectado a la entrada digital E1
+                while (!hayEntrada)
+                {
+                    hayEntrada = fish.GetInput(FishFa30.Nr.E1);
+                }
+                // Una vez que se ha activado el interruptor...
+                if (hayEntrada)
+                {
+                    // Activamos el actuador conectado a la salida digital M1
+                    fish.SetMotor(FishFa30.Nr.M1, FishFa30.Dir.On);
+                    // Esperamos 3000 ms
+                    fish.Pause(3000);
+                    // Paramos el actuador conectado a la salida digital M1
+                    fish.SetMotor(FishFa30.Nr.M1, FishFa30.Dir.Off);
+                }
+                // Cerramos la conexion con el interfaz
+                fish.CloseInterface();
+                Console.ReadLine();
+
+            }
+            catch (FishFa30.FishFaceException)
+            {
+                Console.WriteLine("ERROR:: No se ha podido abrir la interfaz");
+                Console.ReadLine();
+            }
+        }
+    }
+}
