0f1981e4afbbd779408f873586d7124c66f429ab
Txedo <txedo@users.noreply.github.com>
2009-03-31 18:51:32 +0000

    * Añadido ventana "Acerca de..." * Completada la ventana "Acerca de..." * Incorporada la ventana "Acerca de..." a la aplicación ---> La aplicación está completamente funcional, solo falta testearla a fondo en el laboratorio.

---
+++ Practica 4/CheckInterface/CheckInterface/AboutBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace CheckInterface
+{
+    partial class AboutBox : Form
+    {
+        public AboutBox()
+        {
+            InitializeComponent();
+            this.Text = "Acerca de CheckInterface";
+            this.labelProductName.Text = "CheckInterface";
+            this.labelVersion.Text = "Versión 1.0";
+            this.labelCopyright.Text = "Copyleft © 2009 Jose Domingo López y Raul Arias";
+            this.labelCompanyName.Text = "UCLM";
+            this.textBoxDescription.Text = "CheckInterface es un programa con una funcionalidad similar a la herramienta \"Check Interface\" incluida en LLWin.\r\nCheckInterface permite visualizar en todo instante el estado de las entradas de la interfaz y ajustar el estado deseado para cualquiera de las salidas.";
+        }
+    }
+}
