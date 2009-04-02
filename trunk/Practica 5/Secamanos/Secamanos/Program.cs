29dfafac5fc7c61365cdd62e887c1954bd03cee0
txedosuke <txedosuke@1caea46c-0d9b-11de-b321-f573fdaff03e>
2009-04-02 17:21:02 +0000

    Hecha la Practica 5. Falta probarla en el labo.

---
+++ trunk/Practica 5/Secamanos/Secamanos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FishFa30;
+
+namespace Secamanos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Se deben conectar:
+             * lámpara en la salida digital M1
+             * célula óptica en la entrada digital E1
+             * motor en la salida digital M2
+            */
+            Boolean luzDetectada = true;
+            Boolean echo = false;
+            try
+            {
+                // Instanciamos el interfaz FishFace
+                FishFace fish = new FishFace();
+                // Encendemos la lámpara
+                fish.SetLamp(Nr.M1, Dir.On);
+                Console.WriteLine("Iniciando el sistema...");
+                // Esperamos un segundo para que se encienda la lámpara
+                fish.Pause(1000);
+                Console.WriteLine("Sistema iniciado.");
+                while (true)
+                {
+                    luzDetectada = fish.GetInput(Nr.E1);
+                    if (!luzDetectada)
+                    { // Si no se detecta luz es porque hemos pasado la mano
+                        fish.SetMotor(Nr.M2, Dir.On);
+                        echo = !luzDetectada;
+                    }
+                    else if (echo)
+                    {
+                        fish.Pause(5000);
+                        fish.SetMotor(Nr.M2, Dir.Off);
+                        echo = false;
+                    }
+                }
+            }
+            catch (FishFaceException)
+            {
+                Console.WriteLine("ERROR:: No se pudo iniciar el interfaz.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
