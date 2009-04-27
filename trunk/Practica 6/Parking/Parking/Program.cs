82fca9d050f27b7da3c09bcbc223d7b9192a8b3f
txedosuke <txedosuke@1caea46c-0d9b-11de-b321-f573fdaff03e>
2009-04-27 15:02:14 +0000

    Subidos: * Versión entregada de la documentación de la práctica 5 * Los 3 videos entregados de la práctica 5 * Proyecto recién creado de la práctica 6

---
+++ trunk/Practica 6/Parking/Parking/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FishFa30;
+
+namespace Parking
+{
+    class Program
+    {
+        FishFace fish = new FishFace(true, false, 0);
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
